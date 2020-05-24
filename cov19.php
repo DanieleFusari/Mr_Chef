812b4afb87f54a25d8cf428a60ac7fee4ef1ed0f
Daniele Fusari <danieledoughty@Danieles-MacBook-Pro.local>
2020-05-24 20:52:10 +0100

    cov19

--- cov19.php
+++ cov19.php
@@ -1,9 +1,67 @@
+<?php
+$link = "css/cov19.css";
+$title = "Mr Chef Service";
+include('inc/header.php');
+?>
+
+<main>
+
+  <h1 class="header">We are here when you need us!!</h1>
+
+  <p class="p">With restrictions slowly being relaxed and people coming back
+    to the office please remember we are here and we have many options that
+    can be offered that fall in line with COVID-19 guidelines.
+    <br>
+    Our menus already offer a lunch bag option but we are also
+    offering a grab and go station set up for your meetings!!
+  </p>
 
 
+  <div class="bow"><p class="bow_text">£6.00<br><span class="vat"> + Vat</span></p></div>
+  <img class="bow_mobile" src="/img/bow.jpg" alt="bow">
 
-Coronavirus (COVID-19):
+  <img class="lunch_bag_apple" src="img/lunch_bag.png" alt="Lunch bags">
 
-Dear customers due to the Corona Virus, we have a limited sevice until further notice.
-We apologise to all our customers for any inconvenience this will cause.
-Thank you for your understanding during this difficult period.
-Please see the gov.uk web page for more information 
+  <div class="lunch_box">
+    <h4 class="lunch_pack">Lunch pack</h4>
+    <ul class="lunch_items">
+      <li>Pre-Packed Sandwich</li>
+      <li>Bag of Crisps</li>
+      <li>Piece of Fruit</li>
+      <li>Individual Wrapped bar</li>
+      <li>Bottled Water or Can of Pop</li>
+    </ul>
+  </div>
+
+
+  <div class="upgrade">Pick 3 extra items for just <span>£1</span></div>
+  <img class="lunch_bag" src="img/lunch%20bags.png" alt="Lunch bags">
+
+  <div class="lunch_extra">
+    <ul class="extra_items">
+      <h4 class="extra">Extra Items...</h4>
+
+      <li>Mini Quiche</li>
+      <li>Chicken Skewer</li>
+      <li>Chicken Satay</li>
+      <li>Baijis & Samosa (v)</li>
+      <li>Spring Roll (v)</li>
+    </ul>
+  </div>
+
+  <img class="lunch_bag_box" src="img/lunch-box.png" alt="Lunch bags">
+
+  <div class="mm">
+    <ul class="mix_match_items">
+      <h5 class="mix_match">Mix and Match <br> your Lunch Pack</h5>
+      <li>Healthy Bars</li>
+      <li>Fruit Pots</li>
+      <li>Yoghurt Pot</li>
+      <li>Salad Pot</li>
+    </ul>
+
+  </div>
+
+</main>
+
+<?php include('inc/footer.php')?>
